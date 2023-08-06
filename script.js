b6090e546f16cec34d8eae4cde471be07dd8962d
Sangeet Kumar <91060606+skdeveloper8@users.noreply.github.com>
2023-08-06 14:35:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let started=false;
 let level=0;
 let h2=document.querySelector('h2');
 
-document.addEventListener('keypress',function(){
+document.addEventListener('click',function(){
  if(started==false){
     console.log('game starerd');
     started=true;
